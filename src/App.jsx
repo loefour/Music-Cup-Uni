804e60d66242e4529df7a5d5f9d3731603a69f25
Arian <gamer14loefour@gmail.com>
2026-06-23 19:11:29 +0330

    chage to 90 percent.

--- src/App.jsx
+++ src/App.jsx
@@ -597,7 +597,7 @@ export default function Recorder() {
                                     gap: 20,
                                     height: isMobile ? "auto" : 180,
                                     flexShrink: 0,
-                                    width: "100%",
+                                    width: "90%",
                                 }}
                             >
                                 <div
